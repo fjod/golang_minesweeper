41beff863381c69cbd7b1b1bc7935009ab30f548
Michael <fourjustoneday@yandex.ru>
2024-06-18 12:08:38 +0300

    wipwip

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module minesweeper
 
-replace golang_minesweeper/minesweeper => ../minesweeper
-
 go 1.22.3
--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"time"
 )
 
 func main() {
+	fmt.Println("Hello, playground")
 	var game Game
 	game.Init()
 
@@ -12,4 +14,5 @@ func main() {
 		time.Sleep(100 * time.Millisecond)
 		game.ProcessKeyStroke()
 	}
+
 }
